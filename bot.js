af6fc038e17484b610c8bae0c97ce065eb2c9bac
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-11-21 18:04:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const developers = ['342743778106408961','514641715651543041'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -16,15 +17,27 @@ client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Sst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/raaaaaaaaaaaz");
+  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
   client.user.setStatus("idle")
    message.channel.send(` ** Done. :white_check_mark: ** `)
-} else 
+} else
 if (message.content.startsWith('Spl')) {
 client.user.setActivity(argresult , {type:'PLAYING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
 }
+  client.on('message', message => {//-              Ráááááz  , $!#8811 
+        if (!developers.includes(message.author.id)) return;//-              Ráááááz  , $!#8811 
+  if (message.content === '963') {//-              Ráááááz  , $!#8811 
+  
+  let channel = client.channels.get('514641715651543041');//-              Ráááááz  , $!#8811 
 
+  channel.join()//-              Ráááááz  , $!#8811 
+  .then(connection => console.log(`????`))//-              Ráááááz  , $!#8811 
+  .catch(console.error);//-              Ráááááz  , $!#8811 
+  }
+});
+          
+});
 client.login(process.env.BOT_TOKEN);
